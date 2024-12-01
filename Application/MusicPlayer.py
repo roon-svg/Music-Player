84069eabb61d5a527cff470dd7fd827e1026cdea
roon-svg <138370200+roon-svg@users.noreply.github.com>
2024-12-01 14:48:31 +0000

    Setting up the look of the application

---
+++ Application/MusicPlayer.py
@@ -0,0 +1,42 @@
+from tkinter import *
+import pygame
+from static.Playlist import MusicPlays
+from static.LinkedList import LinkedList
+
+# This initialises the tkinter application
+root = Tk()
+root.title("Music Player")
+root.geometry("500x500")
+
+#This gets the music player ready and allows for the song to go an event when the song ends
+pygame.mixer.init()
+Song_End = pygame.USEREVENT + 1
+pygame.mixer.music.set_endevent(Song_End)
+
+def play_music(event=None):
+    tempStorage = MusicPlays()
+    MusicList = LinkedList()
+    for n in range(tempStorage):
+        MusicList.append(tempStorage[n])
+
+    pygame.mixer.music.load(MusicList.get_head_node)
+    pygame.mixer.music.play()
+
+songlist = Listbox(root, background="Navy", foreground="White", width=100, height=25)
+songlist.pack()
+
+control_frame = Frame(root)
+control_frame.pack()
+
+playBtn = Button(control_frame,text=">", command=play_music, width=10)
+pauseBtn = Button(control_frame,text="||", command=play_music, width=10)
+nextSongBtn = Button(control_frame,text=">>", command=play_music, width=10)
+prevSongplayBtn = Button(control_frame,text="<<", command=play_music, width=10)
+ShuffleBtn = Button(control_frame,text="Shuffle", command=play_music, width=10)
+prevSongplayBtn.pack()
+playBtn.pack()
+pauseBtn.pack()
+nextSongBtn.pack()
+ShuffleBtn.pack()
+
+root.mainloop()
--- Application/app.py
+++ Application/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from static.Playlist import MusicPlays
-
+import tkinter
 
 MusicList = MusicPlays()
 
